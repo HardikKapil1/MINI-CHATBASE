412f81c320d1befd13f272b07df3ae3bc8edfdc3
Hardik-T0078 <hardik.k@broadwayinfotech.com>
2026-07-11 23:48:05 +0530

    initialize project structure with FastAPI, add environment and configuration files

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    app_name: str = "Mini Chatbase"
+    app_version: str = "1.0.0"
+    host: str = "127.0.0.1"
+    port: int = 8000
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
+
+
+settings = Settings()
